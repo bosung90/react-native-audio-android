df20b3c41df9f6190e6cd536153dc70c363cc5a3
Eric Kim <eric.kim@conquermobile.com>
2015-11-12 13:10:03 -0800

    make iOS and android both point to index.js

--- android/app/src/main/java/com/myfirstreactnative/MainActivity.java
+++ android/app/src/main/java/com/myfirstreactnative/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends Activity implements DefaultHardwareBackBtnHand
         mReactInstanceManager = ReactInstanceManager.builder()
                 .setApplication(getApplication())
                 .setBundleAssetName("index.android.bundle")
-                .setJSMainModuleName("index.android")
+                .setJSMainModuleName("index")
                 .addPackage(new MainReactPackage())
                 .addPackage(new AudioAndroidPackage())
                 .setUseDeveloperSupport(BuildConfig.DEBUG)
